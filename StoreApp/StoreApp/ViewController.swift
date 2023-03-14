c3625ffa4deac3621e3c63591ee4cd6d8a8e6811
Reynaldo Hutahaean <reynhthaean@gmail.com>
2023-03-14 09:03:25 +0700

    initial commit

---
+++ StoreApp/StoreApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  StoreApp
+//
+//  Created by Reynaldo Cristinus Hutahaean on 14/03/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
